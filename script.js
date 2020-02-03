248fcc314adf4dd6bd8721a2ae020ffa16f7b95f
Nino Chimento <saninoc84@gmail.com>
2020-02-03 12:27:31 +0100

    creo mese con data element giorni

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
-  var gennaio = moment("01-01-2018");
-  console.log(gennaio.format("D-MMMM"));
-  for (var i = 0; i < 31; i++) {
+  var gennaio = moment("2018-01-01");
+  var giorniGennaio = moment(2018-01).daysInMonth();
+  for (var i = 0; i < giorniGennaio; i++) {
     var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
     var context = {
       giorno: parseInt(gennaio.format("D")) + i,
-      mese: gennaio.format("MMMM")
-      };
+      mese: gennaio.format("MMMM"),
+      data : "2018-01" + "-" + addZero((parseInt(gennaio.format("D")) + i)) ,
+    };
     var html = template(context);
     $("ul").append(html);
   }
 });
+function addZero(num) {
+  if (num < 10) {
+    return "0"+num
+  }
+  else {
+    return num
+  }
+}
