ebebeeb237ac822da8d09dee794d14e7aa4f44fe
Nino Chimento <saninoc84@gmail.com>
2020-02-03 13:24:08 +0100

    creo una funzione per la creazione dei mesi con rispettivo data element

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
   var gennaio = moment("2018-01-01");
   var giorniGennaio = moment(2018-01).daysInMonth();
-  for (var i = 0; i < giorniGennaio; i++) {
-    var source = $("#entry-template").html();
-    var template = Handlebars.compile(source);
-    var context = {
-      giorno: parseInt(gennaio.format("D")) + i,
-      mese: gennaio.format("MMMM"),
-      data : "2018-01" + "-" + addZero((parseInt(gennaio.format("D")) + i)) ,
-    };
-    var html = template(context);
-    $(".wrap").append(html);
-  }
+  giorniDelMese(giorniGennaio,gennaio)
+  // for (var i = 0; i < giorniGennaio; i++) {
+  //   var source = $("#entry-template").html();
+  //   var template = Handlebars.compile(source);
+  //   var context = {
+  //     giorno: parseInt(gennaio.format("D")) + i,
+  //     mese: gennaio.format("MMMM"),
+  //     data : "2018-01" + "-" + addZero((parseInt(gennaio.format("D")) + i)) ,
+  //   };
+  //   var html = template(context);
+  //   $(".wrap").append(html);
+  // }
   $.ajax({
     url : "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
     method : "GET",
@@ -36,6 +37,20 @@ $(document).ready(function () {
     }
   });
 });
+function giorniDelMese(numGiorni,mese) {
+  for (var i = 0; i < numGiorni; i++) {
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      giorno: parseInt(mese.format("D")) + i,
+      mese: mese.format("MMMM"),
+      data : mese.format("YYYY"+"-"+"MM") + "-" + addZero((parseInt(mese.format("D")) + i)) ,
+    };
+    var html = template(context);
+    $(".wrap").append(html);
+  }
+}
+// aggiungo zero
 function addZero(num) {
   if (num < 10) {
     return "0"+num
