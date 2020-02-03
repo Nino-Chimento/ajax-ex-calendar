05153d434c0ab8c0ca9684440d8b34e861bcac0e
Nino Chimento <saninoc84@gmail.com>
2020-02-03 10:49:54 +0100

    stampo il mese di gennaio

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-  alert("N")
+  var gennaio = moment("01-01-2018");
+  console.log(gennaio.format("D-MMMM"));
+  for (var i = 0; i < 31; i++) {
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      giorno: parseInt(gennaio.format("D")) + i,
+      mese: gennaio.format("MMMMM")
+      };
+    var html = template(context);
+    $("ul").append(html);
+  }
 });
