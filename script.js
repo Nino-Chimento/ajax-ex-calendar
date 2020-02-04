a2840938f421a0c730cc86f7fcb115e057ec472b
Nino Chimento <saninoc84@gmail.com>
2020-02-04 09:09:45 +0100

    inserimento prompt utente per impegno annuale

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ $(document).ready(function () {
     },
     {
       name:" Festa a caso",
-      date : "2018-01-10"
+      date : "2018-01-10",
+    },
+    {
+      name : prompt("inserisci il nome del tuo impegno"),
+      date : prompt("inserisci la data del tuo impegno esempio 2018-03-09")
+
     }
   ]
   console.log(festivita.length);
@@ -69,40 +74,39 @@ function feste(mese) {
               }
               var html = template(context);
               $(this).append(html)
+            }
+          });
 
-          }
-        });
-
+        }
+      },
+      erorr : function (richiesta,stato,errore) {
+        alert("errore"+errore)
       }
-    },
-    erorr : function (richiesta,stato,errore) {
-      alert("errore"+errore)
+    });
+  }
+  // creo i giorni
+  function giorniDelMese(mese) {
+    var giorniMese = moment(2018-0+mese).daysInMonth();
+    for (var i = 0; i < giorniMese; i++) {
+      var source = $("#entry-template").html();
+      var template = Handlebars.compile(source);
+      var context = {
+        giorno: addZero(i+1),
+        mese: moment().month(mese-1).format("MMM"),
+        data : "2018-"+addZero(mese)+"-"+addZero(i+1),
+        nomeGiorno:moment("2018-"+addZero(mese)+"-"+addZero(i+1)).format("dddd"),
+      };
+      var html = template(context);
+      $(".wrap").append(html);
+      $("h1").text(moment().month(mese-1).format("MMMM"));
     }
-  });
-}
-// creo i giorni
-function giorniDelMese(mese) {
-  var giorniMese = moment(2018-0+mese).daysInMonth();
-  for (var i = 0; i < giorniMese; i++) {
-    var source = $("#entry-template").html();
-    var template = Handlebars.compile(source);
-    var context = {
-      giorno: addZero(i+1),
-      mese: moment().month(mese-1).format("MMM"),
-      data : "2018-"+addZero(mese)+"-"+addZero(i+1),
-      nomeGiorno:moment("2018-"+addZero(mese)+"-"+addZero(i+1)).format("dddd"),
-    };
-    var html = template(context);
-    $(".wrap").append(html);
-    $("h1").text(moment().month(mese-1).format("MMMM"));
   }
-}
-// aggiungo zero
-function addZero(num) {
-  if (num < 10) {
-    return "0"+num
+  // aggiungo zero
+  function addZero(num) {
+    if (num < 10) {
+      return "0"+num
+    }
+    else {
+      return num
+    }
   }
-  else {
-    return num
-  }
-}
